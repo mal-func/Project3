d2bbfd8428f08e8d9a91fa9568f03efc003b560c
mal-func <hk223697@gmail.com>
2022-10-14 12:45:49 +0530

    update html css and js files

--- script.js
+++ script.js
@@ -6,18 +6,35 @@ function compute()
         document.getElementById("principal").focus()
     }
     else{
-        var rate = document.getElementById("rate_val").value;
+    var rate = document.getElementById("rate_val").value;
     var years = document.getElementById("years").value;
     var interest  = (principal*rate*years) /100;
     var amount = Number(principal)+ Number(interest);
     var year = new Date().getFullYear()+parseInt(years);
-    document.getElementById("result").innerHTML="<br>If you deposit <mark>"+principal+"\</mark><br\>at an interest rate of <mark>"+rate+"</mark><br\>You will receive an amount of <mark>"+amount+"</mark>\<br\>in the year <mark>"+year+"</mark>\<br\>";
+
+    document.getElementById("result").innerHTML="Result<br><br>If you deposit &#8377 <mark>"+principal+"\</mark><br\>at an interest rate of <mark>"+rate+"</mark> <br\>You will receive an amount of <mark>"+amount+"</mark>\<br\>in the year <mark>"+year+"</mark>\<br\>";
     }
-    
-      
+}
 
 function updateRate()
 {
     var rateval = document.getElementById("rate_val").value;
     document.getElementById("rate_res").innerText=rateval;
 }
+
+
+
+let clk = document.querySelector(".submit")
+clk.addEventListener('click', (e) => {
+    e.preventDefault();
+    compute();
+})
+
+let btn = document.querySelector('#my-form')
+btn.addEventListener('mouseover', (e) => {
+    document.querySelector("#my-form").style.background = "#ccc"
+})
+btn.addEventListener('mouseout', (e) => {
+    document.querySelector("#my-form").style.background = "#f4f4f4"
+})
+
